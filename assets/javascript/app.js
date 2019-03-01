797becc91408630b37d9de3d9189ce2a34637f8e
Thor Nolan <tnolan5764@gmail.com>
2019-03-01 08:22:42 -0800

    added some timer functionality and finished some styling for question display area

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,76 @@
 $(document).ready(function() {
 // ================GLOBALS=======================
 
+  //variables relating to my questions and the array of questions
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+    var questions = [
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+
+        {question: ,
+        choices: ,
+        correctIndex: ,
+        gif: },
+    ];
+
+  // variables relating to my timer and its' functionality
+    var timer = 30;
+    var timerRunning = false;
+    var intervalId;
+
+  // variables for keeping track of player choices and some empty arrays for pushing choices into
+    var 
+
+
+// ====================FUNCTIONS===================
+
+  // timer function that will decrement the timer by 1 second at a time
 
 
 
 
-
+//
 
 
 
