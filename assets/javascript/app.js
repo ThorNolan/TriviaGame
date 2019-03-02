92941de709fa682e08daf9a36bfde479f0536dea
Thor Nolan <tnolan5764@gmail.com>
2019-03-01 20:27:03 -0800

    finished my timer functions, working on having it display correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
+    var numQestions = questions.length;
 
     var questions = [
-
         // each question has 4 components: the question itself, an array with the 4 different choices, the index of the right answer, and the gif I have in my assets folder that's associated with it
         {question: "Tracer can blink __ times before needing to wait for the ability to recharge.",
         choices: ["14", "3", "5", "7"],
@@ -15,9 +15,9 @@ $(document).ready(function() {
         gif: "assets/images/tracer-juke.gif"},
 
         {question: "Which of the following is NOT one of McCree's voice lines?",
-        choices: ["It's your funeral", "You seem familiar. Ain't I killed you before?", "I'm your Huckleberry", "This town ain't big enough for the two of us"],
+        choices: ["It's your funeral.", "It's high noon!", "I'm your Huckleberry.", "This town ain't big enough for the two of us."],
         correctIndex: 3,
-        gif: ""},
+        gif: "assets/images/mccree-sextuple.gif"},
 
         {question: "Zenyatta moves around by ____.",
         choices: ["levitating", "walking/running", "using psychic powers", "using magnetism"],
@@ -34,25 +34,21 @@ $(document).ready(function() {
         correctIndex: 1,
         gif: "assets/images/road-hook.gif"},
 
-        {question: "",
-        choices: [],
-        correctIndex: ,
-        gif: },
+        {question: "The character Mei is originally from ____.",
+        choices: ["South Korea", "Germany", "China", "The United States"],
+        correctIndex: 2,
+        gif: "assets/images/mei-bye.gif"},
 
-        {question: "",
-        choices: [],
-        correctIndex: ,
-        gif: },
+        {question: "Sombra's name means ____ in spanish.",
+        choices: ["'assassin", "'infiltrator'", "'hacker'", "'shadow'"],
+        correctIndex: 3,
+        gif: "assets/images/sombra-wave.gif"},
 
-        {question: ,
-        choices: ,
-        correctIndex: ,
-        gif: },
+        {question: "How much damage does the quick melee ability do (excluding Reinhardt and Brigitte!)?",
+        choices: ["30 damage", "60 damage", "100 damage", "25 damage"],
+        correctIndex: 0,
+        gif: "assets/images/friendship.gif"},
 
-        {question: "",
-        choices: [],
-        correctIndex: ,
-        gif: },
     ];
 
   // variables relating to my timer and its' functionality
@@ -60,25 +56,51 @@ $(document).ready(function() {
     var timerRunning = false;
     var intervalId;
 
-  // variables for keeping track of player choices and some empty arrays for pushing choices into
+  // variables for keeping track of player choices and some empty arrays for pushing the players' choices into
     var questionHolder = [];
+    var playerSelection = "";
+    var randomSelection = "";
 
 
 // ====================FUNCTIONS===================
 
-  // timer function that will decrement the timer by 1 second at a time
-
+  // timer function that will start my timer and decrement the time by 1 second at a time
+    function startTimer () {
+        if (!timerRunning) {
+            intervalId = setInterval(counter, 1000);
+            timerRunning = true;
+        }
+    }
 
   // function to call when I need to stop the timer
+    function stopTimer () {
+        timerRunning = false;
+        clearInterval(intervalId);
+    }
 
+  // function to display the timer at my #timer div and update it as it counts down to 0 at the interval I set in startTimer()
+  function counter() {
+    timer--;
+    $("#timer").html("<p>" + timer + "</p>");
 
+    if (timer === 0) {
+        stopTimer();
+        unanswered++;
+    }
+  }
+  
 
 
 // =====================GAMEPLAY=====================
 
-    // hide my reset button at the beginning of the game
+  // hide my reset button at the beginning of the game
+    $("#tryAgain")
 
-    // function to start my timer and attach it to my #start button
+
+  // function to start my timer and attach it to my #begin button
+    $("#begin").on("click", function() {
+        startTimer;
+    });
 
 
 
